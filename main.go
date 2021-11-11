b5ea534bc8778dc03e6e44292b71d653475a607a
Julian Alarcon <alarconj@gmail.com>
2021-11-11 17:19:37 -0500

    Check if user input was adquired with no errors

--- main.go
+++ main.go
@@ -18,13 +18,18 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-
 	if len(os.Args) != 2 {
 		printError(errInvalidArguments)
 	}
+
 	originUnit = strings.ToUpper(os.Args[1])
+
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+		_, err := fmt.Scanln(&originValue)
+		if err != nil {
+			printError(errReadingInput)
+		}
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
